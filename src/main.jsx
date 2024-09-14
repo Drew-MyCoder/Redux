29acf0957979635f2fa05287365fbaada63759dc
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-09-14 17:10:41 +0000

    fetchusers and fetchposts on app load

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice.jsx'
+import { fetchPosts } from './features/posts/postsSlice.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </Router>
     </Provider>
   </StrictMode>,
 )
