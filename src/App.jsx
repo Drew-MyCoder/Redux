dc7f7c422ae6a159f58094fae00ac121162b7f99
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-09-15 13:13:54 +0000

    optimization of blog post: userlist, userpage and naviagte addded

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import AddPostForm from './features/posts/AddPostForm'
 import PostsList from './features/posts/PostsList'
 import SinglePostPage from './features/posts/SinglePostPage'
 import Layout from './components/Layout'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import EditPostForm from './features/posts/EditPostForm'
+import UsersList from './features/users/UsersList'
+import UserPage from './features/users/UserPage'
 
 function App() {
 
@@ -22,6 +24,13 @@ function App() {
           <Route path='edit/:postId' element={<EditPostForm />} />
         </Route>
 
+        <Route path='user'>
+          <Route index element={<UsersList />} />
+          <Route path=':userId' element={<UserPage />} />
+        </Route>
+
+        <Route path='*' element={<Navigate to='/' replace />} />
+        
       </Route>
     </Routes>
   )
